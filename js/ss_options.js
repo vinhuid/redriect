5882bf69386364f54e2cdc98344f0c30b2b33c94
vinhuid <vinhuid@outlook.com>
2024-11-07 14:15:16 +0700

    commit

---
+++ js/ss_options.js
@@ -0,0 +1,8 @@
+var ssCountdownDefaultOptions = ssCountdownDefaultOptions || {};
+ssCountdownDefaultOptions.secondsColor 	= 	"#11c8ea";
+ssCountdownDefaultOptions.minutesColor 	= 	"#ff5153";
+ssCountdownDefaultOptions.hoursColor 	= 	"#23d0a2";
+ssCountdownDefaultOptions.daysColor 	= 	"#f9b401";
+ssCountdownDefaultOptions.date 		= 	"2024-12-07";
+ssCountdownDefaultOptions.time 		= 	"22:30:00";
+ssCountdownDefaultOptions.timezone 		= 	"GMT+07:00";
